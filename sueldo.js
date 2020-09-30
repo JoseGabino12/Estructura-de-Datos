8731c0027e50b9fcf7f4bd23101649634261e079
JoseGabino12 <jmorales18@ucol.mx>
2020-09-29 20:56:40 -0500

    First

---
+++ sueldo.js
@@ -0,0 +1,15 @@
+export default class Sueldo{
+    
+    constructor(){
+        var sueldo = 2000;
+    }
+
+    getAumento(){
+        if(){
+            
+        }
+    }
+}
+
+let app = new Sueldo();
+app.getAumento();
