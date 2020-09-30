5c946bde2e548c25a8493feb662d62ffd2543ea0
JoseGabino12 <jmorales18@ucol.mx>
2020-09-29 21:23:11 -0500

    Completed

--- sueldo.js
+++ sueldo.js
@@ -1,15 +1,28 @@
 export default class Sueldo{
     
     constructor(){
-        var sueldo = 2000;
+        this.sueldo = 5000
     }
 
     getAumento(){
-        if(){
-            
+        if(this.sueldo <= 1000){
+            this.sueldo = this.sueldo + (this.sueldo * 15)/100;
+            return this.sueldo;
+        } else if((this.sueldo >= 1001) && (this.sueldo <= 1200) ){
+            this.sueldo = this.sueldo + (this.sueldo * 12)/100;
+            return this.sueldo;
+        }else if((this.sueldo >= 1201) && (this.sueldo <= 1400) ){
+            this.sueldo = this.sueldo + (this.sueldo * 10)/100;
+            return this.sueldo;
+        }else if((this.sueldo >= 1401) && (this.sueldo <= 1500) ){
+            this.sueldo = this.sueldo + (this.sueldo * 8)/100;
+            return this.sueldo;
+        }else if(this.sueldo > 1500){
+            this.sueldo = this.sueldo + (this.sueldo * 5)/100;
+            return this.sueldo;
         }
     }
 }
 
 let app = new Sueldo();
-app.getAumento();
+console.log(app.getAumento());
