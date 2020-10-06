536ba030046557f704d953a862045c6ee0ed6d58
JoseGabino12 <jmorales18@ucol.mx>
2020-10-06 02:36:05 -0500

    Correccion

--- sueldo.js
+++ sueldo.js
@@ -2,17 +2,14 @@ var sueldo = 5000;
 
 if(sueldo <= 1000){
     sueldo = sueldo + (sueldo * 15)/100;
-    console.log(sueldo);
 } else if((sueldo >= 1001) && (sueldo <= 1200) ){
     sueldo = sueldo + (sueldo * 12)/100;
-    console.log(sueldo);
 }else if((sueldo >= 1201) && (sueldo <= 1400) ){
     sueldo = sueldo + (sueldo * 10)/100;
-    console.log(sueldo);
 }else if((sueldo >= 1401) && (sueldo <= 1500) ){
     sueldo = sueldo + (sueldo * 8)/100;
-    console.log(sueldo);
 }else if(sueldo > 1500){
     sueldo = sueldo + (sueldo* 5)/100;
-    console.log(sueldo);
 }
+
+console.log(sueldo);
