b271b3daed4378d2a40b21fdaa248d6fe71b6830
JoseGabino12 <jmorales18@ucol.mx>
2020-10-12 22:35:16 -0500

    Liebre y tortuga

--- liebre-y-tortuga/liebre-y-tortuga.js
+++ liebre-y-tortuga/liebre-y-tortuga.js
@@ -1,5 +1,51 @@
-import Tortuga from  './tortuga.js'
-import Liebre from  './liebre.js'
+class Corredor{
+    constructor(){
+        this.posicion = 0;
+    }
+}
+
+class Liebre extends Corredor{
+    constructor(posicion){
+        super({posicion})
+    }
+
+
+    correr(){ 
+        let avance = Math.ceil(Math.random() * 100);
+
+        if(avance <= 10) {
+            this.posicion -= 12;
+        }else if(avance <= 25){
+            this.posicion -= 2;
+        }else if (avance <= 45) {
+            this.posicion += 0;
+        } else if(avance <= 65){
+            this.posicion += 9
+        }else {
+            this.posicion += 1;  
+        }
+    return this.posicion
+    }
+}
+
+class Tortuga extends Corredor{
+    constructor(posicion){
+        super({posicion})
+    }
+
+    correr(){  
+        let avance = Math.ceil(Math.random() * 100);
+        if(avance <= 20) {
+            this.posicion -= 6;
+        } else if (avance <= 50) {
+            this.posicion += 3;
+        } else if(avance <= 100){
+            this.posicion += 1;  
+        }
+    return this.posicion
+    }
+
+}
 
 class Carrera{
     constructor(){}
@@ -11,16 +57,15 @@ class Carrera{
             
             console.log(`La tortuga va en: ${c1.posicion}, la liebre va en: ${c2.posicion}`)
         }
-
+        
         if((c1.posicion >= 90) && (c2.posicion >= 90)){
             console.log(`Esto ha sido un empate!!`);
         }else if(c1.posicion >= 90){
             console.log(`La tortuga ha ganado!!`);
         }else{
             console.log(`La liebre ha ganado!!`)
-        }
+        }  
     }
-
 
 }
 
@@ -30,3 +75,6 @@ let c2 = new Liebre();
 
 let app = new Carrera();
 app.iniciarCarrera(c1, c2);
+
+
+
